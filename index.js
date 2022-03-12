cf3fcceaab6da5095e181c9849a19f6045b8401b
Andrew <andrew@pureaquasprings.com>
2022-03-12 16:05:03 +1100

    completewithbyenotpassing

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+  if (feet < 400) {
+  return 'This one is on me!';
+   } else if (400 < feet && feet < 2500) {
+  return "I will gladly take your thirty bucks.";
+  } else (feet < 2500) 
+     return 'No can do.';
+   }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+  function ternaryCheckCity(city) {
+    if (city === 'NYC') {
+      return 'Ok, sounds good.';
+    } else 
+       return 'No go.';
+     
+  } // Write your code here!
+
+
+function switchOnCharmFromTip(tip){
+  if  (tip === 'generous') {
+          return  'Thank you so much.';
+   } else (tip === 'not as generous'); {
+     return 'Thank you.';
+  } if  (tip === 'not as generous');{
+  
+  return ' bye';
+   }
 }
